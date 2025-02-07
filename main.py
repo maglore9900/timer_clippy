bf4c8994b279c33e7cdd5c7b404becc6ef4c89b4
maglore9900 <maglore46@gmail.com>
2025-02-06 21:14:28 -0500

    .

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ environ.Env.read_env('config.txt')
 tc = timer_clippy.Clippy(env)
 stop_time = int(env("STOP_TIME").replace(":", ""))
 
+
 while True:
-    now = time.strftime("%I%M", time.localtime())
+    now = time.strftime("%H%M", time.localtime())
     if env("NO_RUN_LIMIT") and int(now) >= stop_time:
         print(f"Time's up!\nSTOP_TIME: {env('STOP_TIME')}")
         time.sleep(3)
